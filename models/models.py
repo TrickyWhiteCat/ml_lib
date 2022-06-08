3143d49cf1b58866c06f21b8a4daf226bf1d4398
TrickyWhiteCat <87902975+TrickyWhiteCat@users.noreply.github.com>
2022-06-08 23:28:02 +0700

    K nearest neighbor (#5)
    * Added KNN model

--- models/models.py
+++ models/models.py
@@ -20,7 +20,7 @@ class Model:
 
     def set_x(self, value):
         self._x = value
-        self._SAMPLE_SIZE = np.size(value, 0)
+        self._NUM_SAMPLES = np.size(value, 0)
         self._SIZE = np.size(value)
         logging.info('x is set')
 
@@ -35,7 +35,7 @@ class Model:
 
     def _scale_feature_x(self):
           # Make sure x is 2D array
-        x = np.reshape(np.copy(self._x), (self._SAMPLE_SIZE, self._SIZE // self._SAMPLE_SIZE))
+        x = np.reshape(np.copy(self._x), (self._NUM_SAMPLES, self._SIZE // self._NUM_SAMPLES))
         if not self._scaling_method: return
         if self._scaling_method == 'standardize':
             self._scaling = preprocess.stardardize(x)
@@ -54,10 +54,10 @@ class Model:
 
     def train_accuracy(self):
         self._accuracy = 0
-        for i in range(self._SAMPLE_SIZE):
+        for i in range(self._NUM_SAMPLES):
             if self.predict(self._x[i]) == self._y[i]:
                 self._accuracy += 1
-        self._accuracy /= self._SAMPLE_SIZE
+        self._accuracy /= self._NUM_SAMPLES
         return self._accuracy
 
 
@@ -91,7 +91,7 @@ class KMeans(Model):
             x = self._x
         c = []
         for i in range(self._num_centroids):
-            c.append(x[np.random.randint(0, self._SAMPLE_SIZE)])
+            c.append(x[np.random.randint(0, self._NUM_SAMPLES)])
         self._centroids = np.array(c)
 
     def _distance_to_centroids(self, x):
@@ -102,14 +102,14 @@ class KMeans(Model):
             x = self._scaling[0]
         except AttributeError:
             x = self._x
-        return np.sum(self._distance_to_centroids(x)) / self._SAMPLE_SIZE
+        return np.sum(self._distance_to_centroids(x)) / self._NUM_SAMPLES
     def _assign(self):
         try:
             x = self._scaling[0]
         except AttributeError:
             x = self._x
-        self._y = np.zeros(self._SAMPLE_SIZE)
-        for i in range(self._SAMPLE_SIZE):
+        self._y = np.zeros(self._NUM_SAMPLES)
+        for i in range(self._NUM_SAMPLES):
             self._y[i] = np.argmin(self._distance_to_centroids(x[i]))
 
     def _update(self):
@@ -197,7 +197,7 @@ class LogisticRegression(Model):
         np.seterr(all='ignore')
 
         # Initialize some constants and parameters
-        SAMPLE_SIZE = self._SAMPLE_SIZE
+        SAMPLE_SIZE = self._NUM_SAMPLES
         lambda_ = self._lambda_
         method = self._method
         num_iters = self._num_iters
@@ -246,3 +246,54 @@ class LogisticRegression(Model):
         return self._CLASSES[id]
 
 
+class KNN(Model):
+    def __init__(self):
+        self._scaling_method = None
+        self._scaled = False
+
+    def set_scaling_method(self, scaling_method):
+        self._scaling_method = scaling_method
+        if (not self._scaled) and self._x.any():
+            self._scale_feature_x()
+            self._scaled = True
+
+    def set_k(self, value):
+        try:
+            self._k = int(value)
+        except ValueError:
+            raise ValueError('k must be an integer')
+
+    def set_x(self, value):
+        self._x = value
+        self._NUM_SAMPLES = np.size(value, 0)
+        self._SIZE = np.size(value)
+        if (not self._scaled) and self._scaling_method:
+            self._scale_feature_x()
+            self._scaled = True
+
+    def set_y(self, value):
+        self._y = value
+        self._CLASSES = np.unique(value)
+        self._NUM_CLASSES = np.size(self._CLASSES)
+
+    def distance(self, x):
+        return np.array([np.linalg.norm(x - c) for c in self._scaling[0]])
+    
+    def predict(self, sample):
+        if self._scaling_method:
+            sample = self._scale_feature(sample.reshape(1, -1))
+        distance = self.distance(sample)
+        order = np.argsort(distance)
+        data = [distance[order][:self._k], self._y[order][:self._k]]
+        k_val = data[0]
+        y = data[1]
+        
+        # Vote for the class
+        class_count = {}
+        for i in range(self._k):
+            if y[i] not in class_count:
+                class_count[y[i]] = np.exp(-k_val[i]**2)
+            else:
+                class_count[y[i]] += np.exp(-k_val[i]**2)
+        return max(class_count, key=class_count.get)
+        
