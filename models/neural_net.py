2b218b32c994f1e61aee738bf72e92935bd9ba53
nmttuan <nmttuan17@gmail.com>
2022-06-09 21:27:28 +0700

    nothing new.

--- models/neural_net.py
+++ models/neural_net.py
@@ -8,4 +8,5 @@ class NN(Model):
     def __init__(self, nodes_per_layer:tuple|list):
         self._NUM_NODES = nodes_per_layer
         self._NUM_LAYERS = len(nodes_per_layer)
-        
+        for i in range(self._NUM_LAYERS - 1):
+            self._weights.append(np.random.rand(self._NUM_NODES[i], self._NUM_NODES[i+1]))
