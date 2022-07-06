fb39768d48a781640d48c2dae03b3a12896b35b4
TrickyWhiteCat <87902975+TrickyWhiteCat@users.noreply.github.com>
2022-07-07 04:24:16 +0700

    Added LinearRegression model
    * initncommit
    
    * minor change to match main branch
    
    * Finalize LinearRegression

--- models/tools/preprocess.py
+++ models/tools/preprocess.py
@@ -3,10 +3,12 @@ import numpy as np
 
 def init_theta(size):
     return np.random.randn(size)
+
 def normalize(x):
     max = x.max(axis = 0).reshape(1, -1)
     min = x.min(axis = 0).reshape(1, -1)
     return np.nan_to_num((x - min) / (max - min)), min, max - min
+
 def standardize(x):
     mean = x.mean(axis = 0).reshape(1, -1)
     std = x.std(axis = 0).reshape(1, -1)
